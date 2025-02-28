a58b70e882c2eb8629cff810095221226e5f4366
Belayne <37399395+Belayne@users.noreply.github.com>
2025-02-28 14:00:36 +0100

    chore: bugfixes
    changed elapsed time to singular when appropriate
    fixed layout for mobile

--- utils/psqlDate.js
+++ utils/psqlDate.js
@@ -2,7 +2,10 @@
 const psqlDate = {
     now: () => new Date().toISOString().slice(0, 19).replace('T', ' '),  //current UTC time in psql timestamp format.
     intervalToString: (interval) => {
-        const string = Object.values(interval)[0] + " " + Object.keys(interval)[0] + " ago";
+        if(Object.keys(interval).length === 0) return "now";
+        const value = Object.values(interval)[0];
+        const key = value == 1? Object.keys(interval)[0].slice(0, -1): Object.keys(interval)[0];
+        const string = value + " " + key + " ago";
         return string;
     }
 }
