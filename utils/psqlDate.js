eb27e23a316c757d3fa5e3da80d45640b57dfd62
Belayne <37399395+Belayne@users.noreply.github.com>
2025-02-28 13:14:22 +0100

    feat: change message date to show elapsed time

--- utils/psqlDate.js
+++ utils/psqlDate.js
@@ -1,6 +1,10 @@
 
 const psqlDate = {
-    now: () => new Date().toISOString().slice(0, 19).replace('T', ' ')  //current UTC time in psql timestamp format.
+    now: () => new Date().toISOString().slice(0, 19).replace('T', ' '),  //current UTC time in psql timestamp format.
+    intervalToString: (interval) => {
+        const string = Object.values(interval)[0] + " " + Object.keys(interval)[0] + " ago";
+        return string;
+    }
 }
 
 module.exports = {
